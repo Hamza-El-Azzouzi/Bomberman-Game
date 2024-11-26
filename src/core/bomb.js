e6eb135d810f06cae90d6d7e0aac1da4185ba600
hkhlifi <khllifihamza66@gmail.com>
2024-11-26 10:05:31 +0100

    bomb placing

--- src/core/bomb.js
+++ src/core/bomb.js
@@ -1,15 +1,16 @@
 let activeBomb = null; 
+const frameWidth = 50;
 const container = document.querySelector('.map');
 
 export function placeBomb(playerState) {
     if (activeBomb) return;
 
-    const bombX = Math.round(playerState.x / 50);
-    const bombY = Math.round(playerState.y / 50);
+    const bombX = Math.round(playerState.x / frameWidth);
+    const bombY = Math.round(playerState.y / frameWidth);
 
     const bomb = document.createElement("div");
     bomb.className = "bomb";
-    bomb.style.transform = `translate3d(${bombX * 50}px, ${bombY * 50}px, 0)`;
+    bomb.style.transform = `translate3d(${bombX * frameWidth}px, ${bombY * frameWidth}px, 0)`;
     container.appendChild(bomb);
 
     activeBomb = bomb;
@@ -24,7 +25,25 @@ export function placeBomb(playerState) {
 function showExplosionEffect(bombX, bombY) {
     const explosion = document.createElement("div");
     explosion.className = "explosion";
-    explosion.style.transform = `translate3d(${bombX * 50}px, ${bombY * 50}px, 0)`;
+    explosion.style.transform = `translate3d(${bombX * frameWidth}px, ${bombY * frameWidth}px, 0)`;
     container.appendChild(explosion);
-    setTimeout(() => explosion.remove(), 1000);
+
+    let frame = 0;
+    let row = 0;
+    const totalFrames = 16;
+    const frameInterval = 100;
+
+    const interval = setInterval(() => {
+        if (frame >= totalFrames) {
+            clearInterval(interval); 
+            explosion.remove();
+            return;
+        }
+        if (frame === 4){
+            row++;
+            frame = 0;
+        }
+        explosion.style.backgroundPosition = `-${frame * frameWidth}px -${row * frameWidth}px`;
+        frame++;
+    }, frameInterval);
 }
