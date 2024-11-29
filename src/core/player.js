1a3e608b7973af03dcd228d8d76a9bff48ab634b
khlifihamza <khllifihamza66@gmail.com>
2024-11-29 21:23:30 +0100

    collision

--- src/core/player.js
+++ src/core/player.js
@@ -15,8 +15,11 @@ const spriteDirections = {
 export const playerState = {
   x: 0,
   y: 0,
+  prevX: 0,
+  prevY: 0,
   speed: 150,
   direction: "down",
+  lastMove: "up-to-down",
   frame: 0,
 };
 
@@ -40,38 +43,79 @@ const frameInterval = 200;
 let lastAnimationTime = 0;
 
 export function update(deltaTime) {
-  const surroundings = utils.checkSurroundings(playerState, Tils, TILE_SIZE);
   const threshold = TILE_SIZE / 2.5;
   let moving = false;
 
+  playerState.prevX = playerState.x;
+  playerState.prevY = playerState.y;
+
+  console.log("Last Move:", playerState.lastMove);
+
+  let row = 1, col = 1
   if (keys.ArrowUp || keys.w) {
     playerState.direction = "up";
-    if (Math.abs(playerState.x % TILE_SIZE) < threshold && surroundings.up) {
-      playerState.y -= Math.round(playerState.speed * deltaTime);
+    row = Math.ceil(playerState.y / TILE_SIZE);
+    if (playerState.lastMove === "left-to-right") {
+      col = Math.ceil(playerState.x / TILE_SIZE);
+    } else if (playerState.lastMove === "right-to-left") {
+      col = Math.floor(playerState.x / TILE_SIZE);
+    }
+    const surroundings = utils.checkSurroundings(row, col, Tils);
+    if (surroundings.up) {
+      if (Math.abs(playerState.x % TILE_SIZE) < threshold) {
+        playerState.y -= Math.round(playerState.speed * deltaTime);
+      }
       playerState.x = Math.round(playerState.x / TILE_SIZE) * TILE_SIZE;
     }
     moving = true;
   }
   if (keys.ArrowDown || keys.s) {
     playerState.direction = "down";
-    if (Math.abs(playerState.x % TILE_SIZE) < threshold && surroundings.down) {
-      playerState.y += Math.round(playerState.speed * deltaTime);
+    row = Math.floor(playerState.y / TILE_SIZE);
+    if (playerState.lastMove === "left-to-right") {
+      col = Math.ceil(playerState.x / TILE_SIZE);
+    } else if (playerState.lastMove === "right-to-left") {
+      col = Math.floor(playerState.x / TILE_SIZE);
+    }
+    const surroundings = utils.checkSurroundings(row, col, Tils);
+    if (surroundings.down) {
+      if (Math.abs(playerState.x % TILE_SIZE) < threshold) {
+        playerState.y += Math.round(playerState.speed * deltaTime);
+      }
       playerState.x = Math.round(playerState.x / TILE_SIZE) * TILE_SIZE;
     }
     moving = true;
   }
   if (keys.ArrowLeft || keys.a) {
     playerState.direction = "left";
-    if (Math.abs(playerState.y % TILE_SIZE) < threshold && surroundings.left) {
-      playerState.x -= Math.round(playerState.speed * deltaTime);
+    if (playerState.lastMove === "up-to-down") {
+      row = Math.ceil(playerState.y / TILE_SIZE);
+    } else if (playerState.lastMove === "down-to-up") {
+      row = Math.floor(playerState.y / TILE_SIZE);
+    }
+    col = Math.ceil(playerState.x / TILE_SIZE);
+    const surroundings = utils.checkSurroundings(row, col, Tils);
+    if (surroundings.left) {
+      if (Math.abs(playerState.y % TILE_SIZE) < threshold) {
+        playerState.x -= Math.round(playerState.speed * deltaTime);
+      }
       playerState.y = Math.round(playerState.y / TILE_SIZE) * TILE_SIZE;
     }
     moving = true;
   }
   if (keys.ArrowRight || keys.d) {
     playerState.direction = "right";
-    if (Math.abs(playerState.y % TILE_SIZE) < threshold && surroundings.right) {
-      playerState.x += Math.round(playerState.speed * deltaTime);
+    if (playerState.lastMove === "up-to-down") {
+      row = Math.ceil(playerState.y / TILE_SIZE);
+    } else if (playerState.lastMove === "down-to-up") {
+      row = Math.floor(playerState.y / TILE_SIZE);
+    }
+    col = Math.floor(playerState.x / TILE_SIZE);
+    const surroundings = utils.checkSurroundings(row, col, Tils);
+    if (surroundings.right) {
+      if (Math.abs(playerState.y % TILE_SIZE) < threshold) {
+        playerState.x += Math.round(playerState.speed * deltaTime);
+      }
       playerState.y = Math.round(playerState.y / TILE_SIZE) * TILE_SIZE;
     }
     moving = true;
@@ -85,6 +129,16 @@ export function update(deltaTime) {
     TILE_SIZE,
     Math.min(mapBounds.height - TILE_SIZE, playerState.y)
   );
+
+  if (playerState.y > playerState.prevY) {
+    playerState.lastMove = "up-to-down";
+  } else if (playerState.y < playerState.prevY) {
+    playerState.lastMove = "down-to-up";
+  } else if (playerState.x > playerState.prevX) {
+    playerState.lastMove = "left-to-right";
+  } else if (playerState.x < playerState.prevX) {
+    playerState.lastMove = "right-to-left";
+  }
 
   if (moving) {
     const currentTime = performance.now();
@@ -103,9 +157,8 @@ export function render() {
   )}px, ${Math.round(playerState.y)}px, 0)`;
 
   const row = spriteDirections[playerState.direction];
-  player.style.backgroundPosition = `-${playerState.frame * frameWidth}px -${
-    row * frameHeight
-  }px`;
+  player.style.backgroundPosition = `-${playerState.frame * frameWidth}px -${row * frameHeight
+    }px`;
 }
 
 function handleKeydown(event) {
--- src/utils/collision.js
+++ src/utils/collision.js
@@ -1,27 +1,12 @@
-export function getPlayerTilePosition(playerState, tileSize) {
-  console.log("worked - getPlayerTilePosition");
-  let row,col = 0
-  if (playerState.direction === "up" || playerState.direction === "left"){
-    row = Math.ceil(playerState.y / tileSize);
-    col = Math.ceil(playerState.x / tileSize);
-  }else{
-    row = Math.floor(playerState.y / tileSize);
-    col = Math.floor(playerState.x / tileSize);
-  }
-  console.log(`Player is at row: ${row}, col: ${col}`);
-  return { row, col };
-}
-
-export function checkSurroundings(playerState, tils, tileSize) {
+export function checkSurroundings(row, col, tils) {
   if (!Array.isArray(tils) || tils.length === 0 || !Array.isArray(tils[0])) {
     throw new Error("Invalid tiles array");
   }
-  console.log(`palyer have those index row {${playerState.y / tileSize}} col {${playerState.x / tileSize}}`)
-  const { row, col } = getPlayerTilePosition(playerState, tileSize);
-  console.log( `can move up - ${tils[row - 1][col]}`)
-  console.log( `can move down - ${tils[row +1][col]}`)
-  console.log( `can move left - ${tils[row][col-1]}`)
-  console.log( `can move right - ${tils[row][col+1]}`)
+  // console.log(`palyer have those index row {${row}} col {${col}}`)
+  // console.log( `can move up - ${tils[row - 1][col]}`)
+  // console.log( `can move down - ${tils[row +1][col]}`)
+  // console.log(`can move left - ${tils[row][col - 1]}`)
+  // console.log( `can move right - ${tils[row][col+1]}`)
 
   return {
     up: row > 0 && tils[row - 1][col] === 0,
