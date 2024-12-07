1e9f866f0f5e61bb61721c7213356a3dc4dc9a4e
khlifihamza <khllifihamza66@gmail.com>
2024-12-07 17:13:48 +0100

    Pause menu (Continue/Restart) Done

--- src/utils/collision.js
+++ src/utils/collision.js
@@ -17,9 +17,9 @@ export function checkSurroundingsBombs(row, col, tils){
   }
 
   return {
-    up: row > 1 && (tils[row - 1][col] === 2 || tils[row - 1][col] === 4 || tils[row - 1][col] === 3),
-    down: row < tils.length - 1 && (tils[row + 1][col] === 2 || tils[row - 1][col] === 4 || tils[row - 1][col] === 3),
-    left: col > 1 && (tils[row][col - 1] === 2 ||  tils[row][col - 1] === 4||  tils[row][col - 1] === 3),
-    right: col < tils[0].length - 1 && (tils[row][col + 1] === 2 || tils[row][col + 1] === 4 || tils[row][col + 1] === 3),
+    up: row > 1 && (tils[row - 1][col] === 2 || tils[row - 1][col] === 4),
+    down: row < tils.length - 1 && (tils[row + 1][col] === 2 || tils[row - 1][col] === 4 ),
+    left: col > 1 && (tils[row][col - 1] === 2 ||  tils[row][col - 1] === 4),
+    right: col < tils[0].length - 1 && (tils[row][col + 1] === 2 || tils[row][col + 1] === 4),
   };
 }
