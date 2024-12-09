5ff3417691612014461f8ccbfde293f740570d63
hamza EL AZZOUZI <elazzouzi.hamza20@gmail.com>
2024-12-09 15:49:22 +0100

    fixing issue in hud&bomb

--- src/utils/hud.js
+++ src/utils/hud.js
@@ -2,7 +2,9 @@ import { isPaused } from "./pause_menu.js";
 
 let score = 0;
 let lives = 5;
-let timer = 0;
+let minutes = 5;
+let seconds = 60;
+let timer = minutes * seconds;
 
 const scoreElement = document.getElementById("score");
 const livesElement = document.getElementById("lives");
@@ -11,12 +13,22 @@ const timerElement = document.getElementById("timer");
 function updateHUD() {
   scoreElement.textContent = `Score: ${score}`;
   livesElement.textContent = `Lives: ${lives}`;
-  timerElement.textContent = `Time: ${Math.floor(timer)}`;
+  minutes = parseInt(timer / 60, 10);
+  seconds = parseInt(timer % 60, 10);
+
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  timerElement.textContent = "game ends in: " + minutes + ":" + seconds;
+
+  if (timer === 0) {
+    gameOver();
+  }
 }
 
 setInterval(() => {
   if (!isPaused) {
-    timer += 1;
+    timer -= 1;
     updateHUD();
   }
 }, 1000);
