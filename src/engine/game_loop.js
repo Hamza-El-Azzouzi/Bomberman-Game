1ef7aa367c90eea853f3bb633405fbca616ab844
khlifihamza <khllifihamza66@gmail.com>
2024-12-23 17:34:54 +0100

    delete width from style.css

--- src/engine/game_loop.js
+++ src/engine/game_loop.js
@@ -22,7 +22,6 @@ export function gameLoop(currentTime, tils) {
   update(deltaTime, tils);
   updateEnemies(deltaTime);
   render();
-  
   if (!isPaused) {
     requestAnimationFrame(gameLoop);
   }
